9ad1fd7dc8a71f2d290a3a4c026fefb244d26b5d
Syaiful <pulbagrup@gmail.com>
2024-12-22 13:08:44 +0700

    update to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import alpinejs from '@astrojs/alpinejs';
+import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
+
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs()],
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
